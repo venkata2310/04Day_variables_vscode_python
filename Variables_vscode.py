6badc47e91e03c35c706baeceed7afe69ed86567
VENKATA RAGURAM KN <107139560+venkata2310@users.noreply.github.com>
2022-06-14 10:53:55 +0530

    Add files via upload

---
+++ Variables_vscode.py
@@ -0,0 +1,17 @@
+# print('Python'.isidentifier())
+# print(' hello'.isidentifier())
+# print('720py'.isidentifier())
+# print('(raw3'.isidentifier())
+# print('a-b'.isidentifier())
+# print('a b'.isidentifier())
+# print('a_b'.isidentifier()) 
+# print('a6b'.isidentifier())
+# print('a6b10'.isidentifier())
+# print('hello23 '.isidentifier())
+
+# '''swapping'''
+# a=5
+# b=10
+# a,b=b,a
+# print(a)
+# print(b)
